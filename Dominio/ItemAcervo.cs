fc1101046fe1732effad06e50e5f44066842d954
Ayllu <ana.l.pereira101@gmail.com>
2026-08-11 19:50:55 -0300

    init: inicialização do projeto, criação da primeira classe em C#, classe abstrata inicial do projetor

---
+++ Dominio/ItemAcervo.cs
@@ -0,0 +1,19 @@
+namespace Biblioteca.Dominio;
+
+public abstract  class ItemAcervo
+{
+  public string Titulo { get; set; }  = string.Empty;
+   public string  Autor { get; set; }  = string.Empty;
+
+   public bool Disponibilidade { get; set; }  = true;
+
+   public abstract int PracoDevolucao {get; set;}
+   public abstract int MultaDiaAtrasado {get; set;}
+
+   public decimal CalcularMulta(int diasAtrasados) {
+    
+    return diasAtrasados >= 0 ? diasAtrasados * MultaDiaAtrasado : 0;
+   } 
+
+
+}
